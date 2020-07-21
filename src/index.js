4eb4fdf73f1f21b68ffb6444198253a6a77c642d
vishakhaagrawal <agrawalvishakha198@gmail.com>
2020-07-21 15:58:33 +0530

    jfp

--- src/index.js
+++ src/index.js
@@ -12,3 +12,19 @@
 //     navbar.classList.remove("sticky");
 //   }
 // }
+$(window).bind("load resize scroll",function(e) {
+    var y = $(window).scrollTop();
+ 
+    $(".back").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' + parseInt(-y / 6) + 'px');
+});
+// $(window).bind("load resize scroll",function(e) {
+//     var y = $(window).scrollTop();
+ 
+//     $(".parallax").filter(function() {
+//         return $(this).offset().top < (y + $(window).height()) &&
+//                $(this).offset().top + $(this).height() > y;
+//     }).css('background-position', '0px ' + parseInt(y /45) + 'px');
+// });
